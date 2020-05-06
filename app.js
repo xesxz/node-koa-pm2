e16be11906789f143b1b38ee9406c77e265b1486
张磊 <631298983@qq.com>
2020-05-06 17:14:53 +0800

    some commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const app = new Koa()
+const router = require('./api/test')
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  console.log(ctx.method, ctx.url, ms);
+})
+app.use(router.routes())
+app.listen(4000, () => {
+  console.log('服务器已经在4000端口运行了');
+})
